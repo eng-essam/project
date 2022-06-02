f0780f5fe080904b787f9c223a3223635ecfd8e0
eng-essam <essamhamdy423@gmail.com>
2022-06-02 18:17:08 +0200

    add vendor file

---
+++ vendor/laravel/fortify/src/Http/Controllers/VerifyEmailController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Laravel\Fortify\Http\Controllers;
+
+use Illuminate\Auth\Events\Verified;
+use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Fortify\Contracts\VerifyEmailResponse;
+use Laravel\Fortify\Http\Requests\VerifyEmailRequest;
+
+class VerifyEmailController extends Controller
+{
+    /**
+     * Mark the authenticated user's email address as verified.
+     *
+     * @param  \Laravel\Fortify\Http\Requests\VerifyEmailRequest  $request
+     * @return \Laravel\Fortify\Contracts\VerifyEmailResponse
+     */
+    public function __invoke(VerifyEmailRequest $request)
+    {
+        if ($request->user()->hasVerifiedEmail()) {
+            return app(VerifyEmailResponse::class);
+        }
+
+        if ($request->user()->markEmailAsVerified()) {
+            event(new Verified($request->user()));
+        }
+          Auth::logout();
+        request()->session()->flash('email_verify', 'تم تأكيد البريد الالكتروني بنجاح');
+         return redirect('login');
+        //return app(VerifyEmailResponse::class);
+    }
+}
